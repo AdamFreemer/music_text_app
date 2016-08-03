43db12c7c831ab9b604c6a407c27087676eee075
Adam Freemer <a@freemer.com>
2016-08-02 23:02:10 -0400

    Added a couple of tests

--- spec/routine_spec.rb
+++ spec/routine_spec.rb
@@ -2,5 +2,21 @@ require "routine"
 
 describe Routine do
 
+	describe "#run_sequence" do
+		context "when @quit is false" do
+			it "should call 'run_sequence'" do
+			  expect(subject).to receive(:run_sequence)
+			  subject.run_sequence
+			end
+		end
+	end
+
+	describe "#show_all" do
+		it "should call 'list_songs'" do
+		  expect(subject).to receive(:list_songs).with([])
+		  subject.show_all
+		end
+	end
+
 
 end
